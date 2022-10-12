b635b3483fcc1e5deadb616b07b4f0929d9dda81
Piotr Lozinski <piotrlozinski9595@gmail.com>
2022-10-12 13:36:38 +0200

    Dodaj pliki projektów.

---
+++ currentDate/currentDate.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h> 
+
+using namespace std;
+
+int main()
+{
+	SYSTEMTIME currentDate;  
+	GetLocalTime(&currentDate); 
+	unsigned int year = currentDate.wYear;
+	unsigned int month = currentDate.wMonth;
+	unsigned int day = currentDate.wDay;
+
+	cout << "Current Year: " << year  << endl;
+	cout << "Current Month: " << month << endl;
+	cout << "Current Day: " << day << endl;
+
+	cout << "Current Date (dd/mm/yyyy): " << day << " / " << month << " / " << year << endl;
+	cout << "Current Date (mm/dd/yyyy): " << month << " / " << day << " / " << year << endl;
+
+	system("pause");
+	return 0;
+}
